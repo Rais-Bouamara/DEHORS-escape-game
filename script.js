9f9943c8086e3ec69816a46870accb6d749174b6
Rais-Bouamara <rais.bouamara@gmail.com>
2025-12-17 17:17:37 +0100

    en travaux

--- script.js
+++ script.js
@@ -1,52 +1,95 @@
 fetch('escapegame.json')
     .then(response => response.json())
-    .then(data => {
+    .then(escapegame => {
 
-        console.log(data)
-        // appeler afficheProduit a l'interieur (ici)
-        afficheRecettes(data)
+        console.log(escapegame)
+
+        afficheAvantagesClients(escapegame)
+        afficheActivites(escapegame)
+        afficheTemoignages(escapegame.temoignages)
 
 
-        data.forEach(element => {
-            
-        });
 
 
     })
+//role : afficher les activité proposer qui sont dans data
+//parametre : data
+//return : rien
+/*section 1 avec les activité proposer*/
+function afficheActivites(escapegame) {
 
-function afficheInformations(listeinformation) {
 
+    escapegame.activites.forEach(activite => {
+        let nom = activite.nom
+        let description = activite.description
+        let image = activite.imageUrl
 
-
-
-
-
-    tableauderecettes.forEach(recette => {
-        let titre = recette.nom
-        let difficulté = recette.difficulte
-        let description = recette.desc
-        let étapes = recette.etapes
-        let image = recette.img
-        let portions = recette.portions
-        let ingredients = recette.ingredients
-        let saison =recette.saison
-        let tempCuisson =recette.tempCuisson
-        let tempPreparation = recette.tempPreparation
 
     });
 
-    tableauderecettes.forEach(recette => {
-document.getElementById("").innerHTML +=`<div class="card mgt100 ">
-                                                 <img src ="${produit.image}" alt="" class="carteimg">
-                                                 <div class="">
-                                                 <h2 class="police">${produit.title}</h2>
-                                                 <p>${produit.category}</p>
-                                                 <p class="prix">${produit.price} euros </p>
-                                                 <p><i class="ph-fill ph-user"></i>${produit.rating.count}</p>
-                                                 <p class ="yellow"><i class="ph-fill ph-star"></i><i class="ph-fill ph-star"></i><i class="ph-fill ph-star"></i><i class="ph-fill ph-star"></i><i class="ph-fill ph-star"></i>${produit.rating.rate}</p>
-                                                 </div>
-                                                 
-                                                 <a href="" class="button2">Acheter</a>
-                                                 </div>`});
+    escapegame.activites.forEach(activite => {
+        document.getElementById("containerActivite").innerHTML += `
+            
+
+                <div class= "buttonred p50 flex space-between mgt100 w25">
+                    <h3>
+                    ${activite.nom}</h3>
+                    <img src="${activite.imageUrl}" alt="" srcset="" class ="w50 bordrond">
+                    <p>${activite.description}</p>
+                </div>
+
+            `});
+
+}
+//role : afficher les témoignage clients qui sont dans data
+//parametre : data
+//return : rien
+/*section 2 avec les temoignage d'escapeur*/
+function afficheTemoignages(temoignages) {
+    
+
+
+    temoignages.forEach(temoignage => {
+        let prenom = temoignage.prenom
+        let typeExperience = temoignage.typeExperience
+        let commentaire = temoignage.commentaire
+        let note = temoignage.note
+
+    });
+
+    temoignages.forEach(temoignage => {
+        document.getElementById("containerTemoignage").innerHTML += `
+            
+                <div class= "buttonred w25 p50 mgt100">
+                    <img src="asset/Ellipse 1.png" alt="" srcset="">
+                    <h3>${temoignage.prenom}</h3>
+                    <h3>${temoignage.typeExperience}</h3>
+                    <p>${temoignage.commentaire}</p>
+                    <h3>${temoignage.note}</h3>
+                </div>`})
+           
+
+}
+
+//role : afficher les avantages clients qui sont dans data
+//parametre : data
+//return : rien
+
+/*section 3 avec les avantages client*/
+function afficheAvantagesClients(data) {
+
+
+    data.avantagesClients.forEach(avantage => {
+
+        document.getElementById("containerAvantage").innerHTML += ` 
+            
+                <div class="w25 p50 cardblack flex space-between mgt100">
+                    <img src="${avantage.img}" alt="" srcset="">
+                    <p>${avantage.phrase}</p>
+                </div>
+                `;
+    });
+
+
 
 }
